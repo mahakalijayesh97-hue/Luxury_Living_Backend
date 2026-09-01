5b9d54e0f742832a12b661e7265d5a3200876ca8
jayesh <mahakalijayesh97@gmail.com>
2026-09-01 14:03:06 +0530

    Fix CORS for frontend connection

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,15 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
